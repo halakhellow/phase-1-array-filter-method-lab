fbb3d0792b00e684ea269c2635c78520e464cf01
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-16 03:12:42 +0300

    add fuzzyMatch function to filter if a driver name starts with a specific passed string

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 let findMatching = (driversNames, name) =>
   driversNames.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+
+let fuzzyMatch = (drivers, str) => {
+  return drivers.filter(
+    (driver) => driver.toLowerCase().indexOf(str.toLowerCase()) === 0
+  );
+};
